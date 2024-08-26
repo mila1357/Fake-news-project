dff4aef5b59bf8613783b2f6e013197a619875e9
mila1357 <136885568+mila1357@users.noreply.github.com>
2024-08-26 17:23:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,3 @@
-# from flask import Flask, request, jsonify, render_template
-# import pickle
-# from flask_cors import CORS
-
-# app = Flask(__name__)
-# CORS(app)
-
-# model = pickle.load(open('Model.pkl', 'rb'))
-# tfidf_vectorizer = pickle.load(open('tfidf_vectorizer.pkl', 'rb'))
-
-# @app.route('/')
-# def home():
-#     return render_template('index.html')
-
-# @app.route('/predict', methods=['POST'])
-# def predict():
-#     text = request.form.get('text')
-#     if text is not None:
-#         text_transformed = tfidf_vectorizer.transform([text])
-
-#         prediction = model.predict(text_transformed)[0]
-
-#         return jsonify({'prediction': int(prediction)})
-#     else:
-#         return jsonify({'error': 'Input text not provided.'})
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-
 from flask import Flask, request, jsonify, render_template
 import pickle
 from flask_cors import CORS
