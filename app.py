c74de7377877a049105e076a2c4018035004f0a1
mydta <tdmy2505@gmail.com>
2024-08-27 20:44:18 +0700

    Display IP Addresses user can use

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 import pickle
 from flask_cors import CORS
+import socket
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
@@ -31,6 +32,26 @@ def predict():
     print(f"Prediction: {prediction}")
     return jsonify({'prediction': int(prediction)})
 
+def list_all_addresses():
+    # Get the hostname
+    hostname = socket.gethostname()
+    
+    # Get the local IP address
+    local_ip = socket.gethostbyname(hostname)
+    
+    # Get all network interfaces (this works for Unix-like systems)
+    try:
+        ip_list = socket.gethostbyname_ex(hostname)[2]
+    except Exception as e:
+        ip_list = [local_ip]
+
+    return ip_list
+
 if __name__ == '__main__':
+    ip_list = list_all_addresses()
+    print("Flask app running! You can access it using any of these addresses:")
+    for ip in ip_list:
+        print(f"http://{ip}:5000")
+        
     app.run(host='0.0.0.0', port=5000) #Flask Server Configuration
 
